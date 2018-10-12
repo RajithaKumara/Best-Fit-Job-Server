2d82bb1842c322539989dd7d2d4f26f5923fde8d
RajithaKumara <tmrkumara.15@cse.mrt.ac.lk>
2018-10-12 20:28:55 +0530

    Update base route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Best Fit Job Server' });
+	try {
+		res.sendFile(path.join(__dirname + '/../public/index.html'));
+	} catch (error) {
+		res.render('index', { title: 'Best Fit Job Server' });
+	}
 });
 
 module.exports = router;
